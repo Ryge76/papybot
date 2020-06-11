9f5b940134bb7378fb303c1c2714d10ff4555848
Ryge76 <ggmaster@msn.com>
2020-06-11 18:41:31 +0200

    UI + input logic in JS

--- static/js/form.js
+++ static/js/form.js
@@ -1,1 +1,12 @@
-const formElt = $("form")
+const formElt = $("#query");
+const questionListElt = $(".test li:first");
+console.log("je suis form.js !")
+
+formElt.change((e)=>{
+    console.error('Je suis bien appelé !')
+    let queryText = "";
+    queryText = $(this).text();
+    questionListElt.insertBefore(queryText);
+})
+
+
