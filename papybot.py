c414f7bdebd104c9af47f4f7d7e73329f11ca393
ryge76 <ggmaster@msn.com>
2020-06-19 19:21:14 +0200

    WIP: modifiying papybot.py (form data)

--- papybot.py
+++ papybot.py
@@ -5,11 +5,19 @@ from . import create_app
 app = create_app()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     if request.method == 'POST':
         return render_template('index.html', key=GMAPS_KEY)
     else:
         return render_template('index.html', key=GMAPS_KEY)
 
+
+@app.route('/search/', methods=['POST'])
+def search():
+    if request.method == 'POST':
+
+        return render_template('index.html', key=GMAPS_KEY)
+    else:
+        return render_template('index.html')
 
