dc323f8c1cf29d62ebbc6057ac60a8fa46ac9ee7
Ryge76 <ggmaster@msn.com>
2020-07-24 15:16:47 +0200

    Mofidied UI.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,5 @@
 import os
 
-base = os.path.abspath(os.path.dirname(__file__))
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 GMAPS_KEY = os.environ.get('GMAPS_KEY')
 FLASK_APP = os.environ.get('FLASK_APP')
